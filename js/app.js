20441b95518e813af224d0f4033d0760ec8d1941
Sandya Sankarram <ss4119@columbia.edu>
2014-11-29 03:03:06 -0500

    register validation

--- js/app.js
+++ js/app.js
@@ -28,9 +28,48 @@ $(document).ready(function() {
                 }
             });
     }
-    
+   $('#register').bootstrapValidator({
+        container: '#registerMessages',
+fields: {
+username: {
+validators: {
+notEmpty: {
+message: 'The username is required and can\'t be empty'
+},
+stringLength: {
+min: 6,
+max: 30,
+message: 'The username must be more than 6 and less than 30 characters long'
+},
+regexp: {
+regexp: /^[a-zA-Z0-9_\.]+$/,
+message: 'The username can only consist of alphabetical, number, dot and underscore'
+},
+different: {
+field: 'password',
+message: 'The username and password can\'t be the same as each other'
+}
+
+}
+},
+password: {
+    validators: {
+        notEmpty: {
+            message: "Password must not be empty"
+        },
+        stringLength: {
+            min: 4,
+            max: 30,
+            message: 'The password must be more than 3 and less than 30 characters long'
+
+        }
+    }
+}
+
+}
+});  
  $('#login').bootstrapValidator({
-container: '#messages',
+        container: '#messages',
 fields: {
 username: {
 validators: {
