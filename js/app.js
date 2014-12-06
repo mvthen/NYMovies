e36099689583e5fb73ff17550ff31ad23cb35a97
Mei-Vern Then <mt2837@columbia.edu>
2014-12-06 12:43:55 -0500

    addd some nytimes ajax call

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 
 
-    var api_key = "hPnzcGaD0tgcmoL6KwVPXoNLMXc8d71l";
+    var mongo_api_key = "hPnzcGaD0tgcmoL6KwVPXoNLMXc8d71l";
+    var nyt_api_key = "e0dc9ba28e7e7c252c51e01eaf637899:6:61350197";
 
     $("#registerBtn").click(function(event) {
         var username = document.getElementById('username').value;
@@ -25,7 +26,7 @@ $(document).ready(function() {
         obj["username"] = username;
         obj["password"] = password;
         $.ajax({ 
-            url: "https://api.mongolab.com/api/1/databases/nytimes_movie/collections/login_info?apiKey=hPnzcGaD0tgcmoL6KwVPXoNLMXc8d71l",
+            url: "https://api.mongolab.com/api/1/databases/nytimes_movie/collections/login_info?apiKey="+mongo_api_key,
             type: "GET",
             contentType: "application/json",
             success: function (data) {
@@ -41,7 +42,7 @@ $(document).ready(function() {
         });
 
         $.ajax({
-                url: "https://api.mongolab.com/api/1/databases/webdev/collections/hits?apiKey=" + api_key,
+                url: "https://api.mongolab.com/api/1/databases/webdev/collections/hits?apiKey=" + mongo_api_key,
                 data: JSON.stringify(obj),
                 type: "POST",
                 contentType: "application/json",
@@ -130,5 +131,68 @@ $(document).ready(function() {
 
         }
     });
+
+    function search_filter(search_type, query) {
+        var queries = query[0];
+        var reviewer = query[1];
+        var opening_date = query[2];
+        var pub_date = query[3];
+        var dvd = query[4];
+        var thousand_best = query[5];
+        var critic_pick = query[6];
+        
+        for (var i = 0; i<query.length; i++) {
+            if (query[i] = "") {
+
+            }
+        }
+        
+        if (search_type == "critic_pick") {
+            search_url = "reviews/all";
+            arg = "";
+        }
+        if (search_type == "reviewer") {
+            search_url = "reviewer/";
+            arg = query;
+        }
+        // if (search_type == "") {
+        //     arg = query;
+        //     query = query.replace("://", "%3A%2F%2F").replace("/", "%2F");
+        //     search_url = "url/exact-match.jsonp?url=" + query + "&api-key=";
+        // }
+
+
+        var message =
+            $.ajax({
+                'url': "http://api.nytimes.com/svc/movies/v2/"+search_url+arg".json?api-key=" + nyt_api_key,
+
+                'type': 'GET',
+                'dataType': "jsonp",
+                success: function(data, textStats, XMLHttpRequest) {
+                    // display_first(data, search_type, arg);
+                    console.log(data);
+                },
+                error: function(data, textStatus, errorThrown) {
+                    console.log("error");
+                }
+            });
+    }
+
+    function reviewer_details(reviewer) {
+        var message =
+            $.ajax({
+                'url': "http://api.nytimes.com/svc/movies/v2/critics/"+reviewer+".json?api-key="+ nyt_api_key,
+
+                'type': 'GET',
+                'dataType': "jsonp",
+                success: function(data, textStats, XMLHttpRequest) {
+                    // display_first(data, search_type, arg);
+                    console.log(data);
+                },
+                error: function(data, textStatus, errorThrown) {
+                    console.log("error");
+                }
+            });
+    }
 
 });
